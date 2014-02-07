0b7ad6ae8ab1a7844313b797b344af33cdd4a862
gupta-kanav <kgupta@seasiainfotech.com>
2014-02-07 15:06:53 +0530

    Initial scope

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 #config.assets.initialize_on_precompile = false
-#require 'rails/all'
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
